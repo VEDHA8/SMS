e7fb673b2e93fd3c39b1fa40a0992d97793242c6
Rathnayake M.R.T.N <99705847+IT21253294tharindu@users.noreply.github.com>
2024-05-07 13:41:39 +0530

    frontend init

---
+++ Frontend/src/menu-items/index.jsx
@@ -0,0 +1,17 @@
+import dashboard from './dashboard';
+import application from './application';
+import forms from './forms';
+import RMS from './rms';
+import samplePage from './sample-page';
+import CMS from './cms';
+import utilities from './utilities';
+import support from './support';
+import other from './other';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+    items: [dashboard, RMS, CMS, application, forms, samplePage, utilities, support, other]
+};
+
+export default menuItems;

f916e70ef8a22e16d14cff419132f01244b863ad
TmApAZ <dttmapatuna@gmail.com>
2024-05-07 13:15:20 +0530

    UI part

---
+++ src/menu-items/index.jsx
@@ -0,0 +1,18 @@
+import dashboard from './dashboard';
+import application from './application';
+import forms from './forms';
+import elements from './elements';
+import samplePage from './sample-page';
+import pages from './pages';
+import utilities from './utilities';
+import support from './support';
+import other from './other';
+import dashboard1 from './rms';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+    items: [dashboard, dashboard1, application, forms, elements, samplePage, pages, utilities, support, other]
+};
+
+export default menuItems;
